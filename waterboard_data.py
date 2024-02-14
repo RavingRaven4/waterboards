6f27d8444062e8b5c58668f8d274d65bd63e7023
Raven <andy_pete@outlook.com>
2024-02-14 14:02:31 -0700

    loop hidden

--- waterboard_data.py
+++ waterboard_data.py
@@ -30,13 +30,38 @@ received = []
 uploaded = []
 link = []
 
-k = 'https://mvlwb.com/registry/mv2001l2-0008'
+k = 'https://mvlwb.com/registry/w2014l3-0002'
 
 # for k in permits:
 response = requests.get(k)
 response.raise_for_status()
 soup = BeautifulSoup(response.text, 'html.parser')
-div_element = soup.find('div', class_=div_class)
+div_element = soup.find_all("div", {"class": "view-content"})
+
+if soup.find_all("div", {"class": "view-content"}):
+    print("Tag Found")
+else:
+    print("Not found")
+
+stew = []
+i = 0
+while True:
+    response = requests.get(k + '?page=' + str(i))
+    response.raise_for_status()
+    soup = BeautifulSoup(response.text, 'html.parser')
+    div_element = soup.find('div', class_=div_class)
+    if div_element == None:
+        break
+    # div_element = soup.find_all("div", {"class": "view-content"})
+    stew.append(div_element)
+    i += 1
+
+len(stew)
+
+
+
+
+
 if div_element:
     links = div_element.find_all('a', href=True)
     for x1 in range(len(links)):
@@ -50,30 +75,9 @@ if div_element:
     for x5 in range(len(links)):
         link.append(links[x5]['href'])
         file_ext.append(link[-1].split('.')[-1])
-    if len(links) > 90:
-        driver = webdriver.Chrome()
-        wait = WebDriverWait(driver, 7)
-        driver.get(k + '?page=1')
-        # driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        # time.sleep(4)
-        # next_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, 'a[title="Go to next page"]')))
-        # driver.execute_script("arguments[0].scrollIntoView(true);", next_button)
-        # next_button.click()
-        html = driver.page_source
-        soup2 = BeautifulSoup(html, 'html.parser')
-        div_element2 = soup2.find('div', class_=div_class)
-        links2 = div_element2.find_all('a', href=True)
-        for x1_1 in range(len(links2)):
-            permit.append(k.split('y/', 1)[1])
-        for x2_1 in links2:
-            title.append(x2_1.getText())
-        for x3_1 in div_element2.select('td[class*="received"]'):
-            received.append(x3_1.getText())
-        for x4_1 in div_element2.select('td[class*="uploaded"]'):
-            uploaded.append(x4_1.getText())
-        for x5_1 in range(len(links2)):
-            link.append(links2[x5_1]['href'])
-            file_ext.append(link[-1].split('.')[-1])
+
+
+
 
 
 
@@ -91,7 +95,7 @@ else:
 
 df = pd.DataFrame({'permit': permit, 'title': title, 'file_ext': file_ext, 'received': received,
                       'uploaded': uploaded, 'link': link})
-df.to_excel("test1.xlsx", sheet_name="TheSauce", index=False)
+df.to_excel("test2.xlsx", sheet_name="TheSauce", index=False)
 
 
 
